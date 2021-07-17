8c8a564c74fda21ba4acd1f6503d679b9ff05636
Ijeamaka Anyene <Ijeamaka.Anyene@gmail.com>
2021-07-16 22:32:32 -0700

    Changing formatting of cards - getting rid of unnecessary code

--- _site/R/functions.R
+++ _site/R/functions.R
@@ -1,15 +1,13 @@
 create_proj_card = function(img_src, url, title, text, text2){
   tags$div(
     class = "card",
-    tags$img(class="card-img-top",
-             src=img_src,
-             style="width:300px;"),
+    tags$img(
+      class = "card-img-top", 
+      src = img_src),
     tags$div(
-      class = "card-body",
-      h4(
-        a(href = url,  
-          title)),
-      p(text, br(), text2)
+        class = "card-body",
+        h4(a(href = url,  title)),
+        p(text, br(), text2)
     )
   )
   
